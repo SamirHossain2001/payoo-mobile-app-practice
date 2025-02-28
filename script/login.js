15a99a2cd7da4ee2917eb34e27c4588e6516ae53
SamirHossain2001 <2001samirhossain@gmail.com>
2025-02-28 21:07:36 +0600

    Login

--- script/login.js
+++ script/login.js
@@ -1,2 +1,34 @@
 
 // alert('Hiiiiiiiiii');
+
+document.getElementById("login-btn")
+    .addEventListener('click', function(event){
+        // stop reload
+        event.preventDefault();
+
+        const accNumber = document.getElementById("acc-number").value;
+        console.log(accNumber); 
+
+        const pin = document.getElementById("pin").value;
+        console.log(pin);
+
+        // Check 11 digit
+        if(accNumber.length === 11){
+            if(pin === '1234'){
+                window.location.href = "main.html";
+            }
+            else{
+                alert("Wrong pin");
+            }
+            console.log('11 found');
+        }
+        else{
+            alert('Enter 11 digit number.')
+        }
+
+
+        // const numberEle = document.getElementById()
+        // const number = numberEle.value;
+        // console.log(number);
+    });
+
