2b6e969bedeb20ccf82ab08eb10f3757f5a80125
SamirHossain2001 <2001samirhossain@gmail.com>
2025-03-06 20:21:37 +0600

    transfer new

--- script/addMoney.js
+++ script/addMoney.js
@@ -20,12 +20,15 @@ document
 
       const pin = document.getElementById("pin").value;
       if (pin === "1234") {
-        // console.log('addd');
+        if (selectedBnak === "") {
+          alert("Select a Bank!!!");
+          return;
+        }
         const sum = mainBalance + amount;
         document.getElementById("main-balance").innerText = sum;
 
         // transection history
-        const container = document.getElementById("transection-container");
+        // const container = document.getElementById("transection-container");
         // const p = document.createElement('p');
         // p.innerText = `
         //   added ${amount} from ${accNumber} account
@@ -33,14 +36,13 @@ document
         // console.log(p);
         // container.appendChild(p);
         const div = document.createElement("div");
-        div.classList.add("bg-blue-200","p-4","border");
+        div.classList.add("bg-blue-200", "p-4", "border");
         div.innerHTML = `
         <h1 class="text-yellow-700">Added Money from ${selectedBnak}</h1>
         <h3>${amount}</h3>
         <p>account no: ${accNumber}</p>
-        `
-        container.appendChild(div);
-
+        `;
+        // container.appendChild(div);
       } else {
         alert("Wrong pin");
       }
@@ -52,7 +54,6 @@ document
 document.getElementById("logout-btn").addEventListener("click", function () {
   window.location.href = "index.html";
 });
-
 
 // document.getElementById("").addEventListener("submit", function(event) {
 //     let bankSelect = document.getElementById("all-bank");
--- script/toggle.js
+++ script/toggle.js
@@ -1,23 +1,30 @@
-
 // Show nothing by default
-// handleToggle("addmoney","none");
-// handleToggle("cashout","none");
+handleToggle("history", "none");
+handleToggle("addmoney", "none");
+handleToggle("cashout", "none");
+handleToggle("transfer", "none");
 // handleToggle("transection-history", "none");
 
 document.getElementById("add-money-box").addEventListener("click", function () {
   handleToggle("addmoney", "block");
-  // handleToggle("cashout", "none");
-  // handleToggle("transection-history", "none");
+  handleToggle("cashout", "none");
+  handleToggle("transfer", "none");
   // document.getElementById("addmoney").style.display = "block";
 });
 
-// document.getElementById("cashout-box").addEventListener("click", function () {
-//   handleToggle("addmoney", "none");
-//   handleToggle("cashout", "block");
-//   handleToggle("transection-history", "none");
-// });
+document.getElementById("cashout-box").addEventListener("click", function () {
+  handleToggle("cashout", "block");
+  handleToggle("addmoney", "none");
+  handleToggle("transfer", "none");
+  // handleToggle("transection-history", "none");
+});
 
-function handleToggle(id, status){
+document.getElementById("transfer-box").addEventListener("click", function () {
+  handleToggle("transfer", "block");
+  handleToggle("addmoney", "none");
+  handleToggle("cashout", "none");
+});
+
+function handleToggle(id, status) {
   document.getElementById(id).style.display = status;
 }
-
