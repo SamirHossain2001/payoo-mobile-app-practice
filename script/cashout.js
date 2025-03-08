b9c236b494f173b913b456d2a421015d2d4ac6e3
SamirHossain2001 <2001samirhossain@gmail.com>
2025-03-08 23:18:52 +0600

    cashout fun

--- script/cashout.js
+++ script/cashout.js
@@ -1,39 +1,38 @@
-// document
-//   .getElementById("cashout-btn")
-//   .addEventListener("click", function (event) {
-//     event.preventDefault();
+document
+  .getElementById("cashout-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
 
-//     const accNumber = document.getElementById("acc-number").value;
+    const accNumber = document.getElementById("acc-number").value;
 
-//     const pinStr = document.getElementById("cashout-pin").value;
-//     const pin = parseInt(pinStr);
+    const pinStr = document.getElementById("cashout-pin").value;
+    const pin = parseInt(pinStr);
 
-//     const amountStr = document.getElementById("cashout-amount").value;
-//     let amount = parseFloat(amountStr);
-//     if (isNaN(amount)) amount = 0;
+    const amountStr = document.getElementById("cashout-amount").value;
+    let amount = parseFloat(amountStr);
+    if (isNaN(amount)) amount = 0;
 
-//     const mainBalanceStr = document.getElementById("main-balance").innerText;
-//     const mainBalance = parseFloat(mainBalanceStr);
+    const mainBalanceStr = document.getElementById("main-balance").innerText;
+    const mainBalance = parseFloat(mainBalanceStr);
 
-//     if (pin === 1234) {
-//       if (mainBalance - amount < 0) {
-//         alert("Don't have enough money");
-//       } else {
-//         const sum = mainBalance - amount;
-//         document.getElementById("main-balance").innerText = sum;
+    if (pin === 1234) {
+      if (mainBalance - amount < 0) {
+        alert("Don't have enough money");
+      } else {
+        const sum = mainBalance - amount;
+        document.getElementById("main-balance").innerText = sum;
 
-//         // transection history
-//         const p = document.createElement("p");
-//         p.innerText = `
-//            cashout ${amount} from ${accNumber} account
-//         `;
-//         console.log(p);
-//         const container = document.getElementById("transection-container");
-//         container.appendChild(p);
-
-//       }
-//     } else {
-//       alert("Wrong pin");
-//     }
-//     document.getElementById("cashout-amount").value = "";
-//   });
+        // transection history
+        // const p = document.createElement("p");
+        // p.innerText = `
+        //    cashout ${amount} from ${accNumber} account
+        // `;
+        // console.log(p);
+        // const container = document.getElementById("transection-container");
+        // container.appendChild(p);
+      }
+    } else {
+      alert("Wrong pin");
+    }
+    document.getElementById("cashout-amount").value = "";
+  });
